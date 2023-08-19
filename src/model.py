a4a1586ef2543e3c5217cf4217bf6d9219b2e212
bhimrazy <bhimrajyadav977@gmail.com>
2023-08-19 23:54:35 +0545

    updates loss

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import torch
 from lightning import LightningModule
-from monai.losses import DiceLoss, DiceFocalLoss
+from monai.losses import DiceLoss, DiceFocalLoss, GeneralizedDiceLoss
 from monai.metrics import DiceMetric
 from monai.networks.nets import UNet
 
@@ -18,7 +18,7 @@ class RudrakshaSegModel(LightningModule):
             num_res_units=2,
         )
         # self.loss_fn = DiceLoss(sigmoid=True)
-        self.loss_fn = DiceFocalLoss(sigmoid=True)
+        self.loss_fn = GeneralizedDiceLoss(sigmoid=True)
         self.metric = DiceMetric(include_background=False, reduction="mean")
 
         self.validation_step_outputs = []
